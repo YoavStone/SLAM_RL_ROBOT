404c63048712fb5d017dd0ca5d92b0c284f1dc6a
Yoav Stone <yoav.stone@gmail.com>
2025-03-24 18:50:44 +0200

    launch lidar

---
+++ RL_robot/launch/rpi4_robot_launch.py
@@ -0,0 +1,80 @@
+#############################################################
+#     ROS2 && Robot Launch File of the diff drive robot     #
+#############################################################
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+import xacro
+
+
+def generate_launch_description():
+
+    # name in xacro file
+    robotXacroName = 'mapping_robot'
+
+    # pkg name to define the paths
+    namePackage = 'RL_robot'
+
+    # path to xacro file
+    modelFileRelativePath = 'model/robot.xacro'
+
+    # absolute path to model
+    pathModelFile = os.path.join(get_package_share_directory(namePackage), modelFileRelativePath)
+
+    # get robot description from xacro model file && combine with .gazebo
+    robotDescription = xacro.process_file(pathModelFile).toxml()
+
+    # Async toolbox SLAM parameters file
+    absPathParamSLAM = os.path.join(get_package_share_directory(namePackage),
+                                    'robot_controller/mapper_params_online_async.yaml')
+
+    # Spawn model GZ node
+    spawnModelNodeGazebo = Node(
+        package = 'ros_gz_sim',
+        executable = 'create',
+        arguments = [
+            '-name', robotXacroName,
+            '-topic', 'robot_description'
+        ],
+        output = 'screen',
+    )
+
+    # Robot State Publisher node
+    nodeRobotStatePublisher = Node(
+        package = 'robot_state_publisher',
+        executable = 'robot_state_publisher',
+        output = 'screen',
+        parameters = [{'robot_description': robotDescription,
+        'use_sim_time': True}]
+    )
+
+    slam_toolbox_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [os.path.join(get_package_share_directory('slam_toolbox'), 'launch', 'online_async_launch.py')]),
+        launch_arguments={'use_sim_time': 'true', 'slam_params_file': absPathParamSLAM}.items()
+    )
+
+    # RViz
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', os.path.join(get_package_share_directory(namePackage), 'rviz', 'gpu_lidar_bridge.rviz')],
+    )
+
+
+    # empty launch description object
+    launchDescriptionObject = LaunchDescription()
+
+    #launchDescriptionObject.add_action(spawnModelNodeGazebo)  # delete?
+    launchDescriptionObject.add_action(nodeRobotStatePublisher)
+
+    launchDescriptionObject.add_action(rviz)
+
+    launchDescriptionObject.add_action(slam_toolbox_launch)
+
+    return launchDescriptionObject
