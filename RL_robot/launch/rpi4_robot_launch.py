73a61c2522cf54f8312f9b6f01fa1647c0f4fd25
Yoav Stone <yoav.stone@gmail.com>
2025-03-27 17:08:04 +0200

    i think slam should work now

--- RL_robot/launch/rpi4_robot_launch.py
+++ RL_robot/launch/rpi4_robot_launch.py
@@ -17,27 +17,9 @@ def generate_launch_description():
     # pkg name to define the paths
     namePackage = 'RL_robot'
 
-    # path to xacro file
-    modelFileRelativePath = 'model/robot.xacro'
-
-    # absolute path to model
-    pathModelFile = os.path.join(get_package_share_directory(namePackage), modelFileRelativePath)
-
-    # get robot description from xacro model file && combine with .gazebo
-    robotDescription = xacro.process_file(pathModelFile).toxml()
-
     # Async toolbox SLAM parameters file
     absPathParamSLAM = os.path.join(get_package_share_directory(namePackage),
                                     'robot_controller/mapper_params_online_async.yaml')
-
-    # Robot State Publisher node
-    nodeRobotStatePublisher = Node(
-        package = 'robot_state_publisher',
-        executable = 'robot_state_publisher',
-        output = 'screen',
-        parameters = [{'robot_description': robotDescription,
-        'use_sim_time': True}]
-    )
 
     slam_toolbox_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -49,14 +31,12 @@ def generate_launch_description():
     rviz = Node(
         package='rviz2',
         executable='rviz2',
-        arguments=['-d', os.path.join(get_package_share_directory(namePackage), 'rviz', 'gpu_lidar_bridge.rviz')],
+        arguments=['-d', os.path.join(get_package_share_directory(namePackage), 'rviz', 'physical_robot.rviz')],
     )
 
 
     # empty launch description object
     launchDescriptionObject = LaunchDescription()
-
-    launchDescriptionObject.add_action(nodeRobotStatePublisher)
 
     launchDescriptionObject.add_action(rviz)
 
--- rpi_comms_base/rpi_comms_base/lidar_launch.py
+++ rpi_comms_base/rpi_comms_base/lidar_launch.py
@@ -1,20 +1,31 @@
 import os
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    serial_port_arg = DeclareLaunchArgument(
+        'serial_port',
+        default_value='/dev/ttyUSB0',  # Provide a default
+        description='Path to the serial port for the RPLIDAR'
+    )
+
+    serial_port = LaunchConfiguration('serial_port')
+
+    rplidar_node = Node(
+        package='rplidar_ros',
+        executable='rplidar_composition',
+        output='screen',
+        parameters=[{
+            'serial_port': serial_port,
+            'frame_id': 'base_footprint',
+            'angle_compensate': True,
+            'scan_mode': 'Standard'
+        }]
+    )
 
     return LaunchDescription([
-
-        Node(
-            package='rplidar_ros',
-            executable='rplidar_composition',
-            output='screen',
-            parameters=[{
-                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
-                'frame_id': 'base_footprint',
-                'angle_compensate': True,
-                'scan_mode': 'Standard'
-            }]
-        )
+        serial_port_arg,
+        rplidar_node
     ])
