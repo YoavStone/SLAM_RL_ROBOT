a13c485e880ddff131105c796b15e1c8807b3f70
Yoav Stone <yoav.stone@gmail.com>
2025-05-02 17:07:03 +0300

    rmv unnecessary imports from tf broadcaster robot

--- rpi_comms_base/rpi_comms_base/TFBroadCasterRobot.py
+++ rpi_comms_base/rpi_comms_base/TFBroadCasterRobot.py
@@ -3,20 +3,13 @@ from rclpy.node import Node
 from nav_msgs.msg import Odometry
 from tf2_ros import TransformBroadcaster
 from geometry_msgs.msg import TransformStamped
-import math
-import numpy as np
-from transforms3d.euler import quat2euler, euler2quat
-from transforms3d.quaternions import qmult
 
 
 class TFBroadCasterRobot(Node):
     def __init__(self):
         super().__init__('tf_board_cast_node_for_robot')
-        self.tf_broadcaster = TransformBroadcaster(self)
 
-        # Create a rotation quaternion for 180 degrees around Z axis
-        # Order in transforms3d is [w, x, y, z]
-        self.rot_180_z = euler2quat(0, 0, math.pi)
+        self.tf_broadcaster = TransformBroadcaster(self)
 
         self.odom_subscription = self.create_subscription(
             Odometry,
